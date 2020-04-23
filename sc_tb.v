45713183506a80b2c6afe0e7ae593a24179bfdf7
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-04-23 19:54:19 +0200

    Update sc_tb.v

--- sc_tb.v
+++ sc_tb.v
@@ -1,32 +1,31 @@
-`include "sc.v"
-
-`timescale 1ns/1ps
-
+ `include "sc.v"
+`timescale 1ns / 1ps
 module sc_tb();
-
-reg clock;
-reg enable;
-reg reset;
-
-wire[3:0] counter;
-
-always #10 clock = ~clock;
-
-  sc counter_test(.clk(clock), .en(enable), .reset(reset), .r_reg(counter[3:0]));
-
-initial
-	$display("time \t\t clk \t en \t reset \t counter",);
-	$monitor("%4d ns \t %b \t %b \t %b \t %b",$time, clock, enable, reset,counter);
+  reg clock;
+  reg rst;
+  reg enable;
+  wire [3:0] counter;
   
-	clock = 0;
-	enable = 0;
-	reset = 1;
-
-	#20  enable = 1; reset = 0;
-	#100 enable = 1; reset = 1;
-	#10  enable = 1; reset = 0;
-	#10  enable = 0; reset = 0;
-	#40  enable = 1; reset = 0;
-	#400 $finish;
-end
+  
+  always #10 clock =~clock;
+  sc U0(.en(enable),.clk(clock),.reset(rst),.r_reg(counter[3:0]));
+  initial
+    begin
+      $display("time \t\t clk \t en \t rst\t counter");
+      $monitor("%4d ns  \t %b\t %b\t %b\t %b",$time,clock,enable,rst,counter);
+      clock=0;
+      enable=0;
+      rst=1;
+     
+      
+      #20 enable=1; rst=0;
+      #20 enable=1; rst=1;
+      #20 enable=1; rst=0;
+      #20 enable=0; rst=0;
+      #20 enable=1; rst=0;
+      
+      #100 $finish;
+      
+      
+    end
 endmodule
