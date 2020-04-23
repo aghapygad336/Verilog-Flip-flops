7aa3a01adc43f067860526f065178389c01473dd
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-04-23 02:14:50 +0200

    Update ff_tb.v

--- ff_tb.v
+++ ff_tb.v
@@ -1,3 +1,4 @@
+
 `include "ff.v"
 module ff_tb ();
 reg d_in;
@@ -18,8 +19,15 @@ begin
     enable_=0;
     clock=0;
     clear_=1;
-    $display("time\tclock\tclear\tenable\tFlipFlop\tOutput");
+    $display("time\t clock \t clear \tenable \t FlipFlop \t Output");
     $monitor(clock,clear_,enable_,d_in,q_outer);
-    
+    #20 enable_ = 1;
+    #20 clear =0;
+    #20 d_in =1;
+    #20 d_in=0;
+    #20 enable_=0;
+    #20 clear =0;
+    #20 d_in =1;
+    #20 d_in=0;
 end
 endmodule
